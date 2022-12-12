afa9ce9e56da7afade0f4a740bcd8ceeed37e9b6
ramontcruz <115509915+ramontcruz@users.noreply.github.com>
2022-12-12 18:05:25 -0300

    CRUD básico de Termos em termo.routes

--- middlewares2/isAuth.js
+++
@@ -1,9 +0,0 @@
-import { expressjwt } from "express-jwt";
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-export default expressjwt({
-  secret: process.env.TOKEN_SIGN_SECRET,
-  algorithms: ["HS256"],
-});
--- routes/termo.routes.js
+++ routes/termo.routes.js
@@ -1,122 +1,82 @@
 import express from "express";
-import { generateToken } from "../config/jwt.config.js";
-import isAuth from "../middlewares/isAuth.js";
-import attachCurrentUser from "../middlewares/attachCurrentUser.js";
-import { isAdmin } from "../middlewares/isAdmin.js";
-import { UserModel } from "../model/user.model.js";
+import { termoModel } from "../models/termo.model.js";
 
-import bcrypt from "bcrypt";
+const termoRoute = express.Router();
 
-const SALT_ROUNDS = 10;
-
-const termoRouter = express.Router();
-
-//Get em todos os termos
-termoRouter.get("/all-termos", async (req,res) => {
+//CREATE termo
+termoRoute.post("/new-termo", async (req,res) => {
     try {
-        const users = await termoModel.find({}, { __v: 0, updatedAt: 0 })
-      .sort({
-        termo: 1,
-      })
-      return res.status(201).json();
+        const createdTermo = await termoModel.create({
+            ...req.body,
+          });
+          return res.status(201).json(createdTermo);
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
     }
 });
 
+//READ em todos os termos
+termoRoute.get("/all-termos", async (req,res) => {
+    try {
+        const termos = await termoModel.find({})
+      .sort({
+        termo: 1,
+      })
+      return res.status(201).json(termos);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+});
+
+//READ termo específico
 termoRoute.get("/termo/:id", async (req, res) => {
     try {
       const { id } = req.params;
   
       // const user = await UserModel.find({_id: id})
-      const user = await UserModel.findById(id).populate("tasks");
-  
-      if (!user) {
-        return res.status(400).json({ msg: " Usuário não encontrado!" });
+      const termo = await termoModel.findById(id);
+      console.log("termo ",termo);
+      if (!termo) {
+        return res.status(400).json({ msg: " Termo não encontrado!" });
       }
-  
-      return res.status(200).json(user);
+      return res.status(200).json(termo);
     } catch (error) {
       console.log(error);
       return res.status(500).json(error.errors);
     }
   });
 
-//Get termo específico
+//UPDATE termo
+termoRoute.put("/edit/:idTermo", async (req, res) => {
+    try {
+      const { idTermo } = req.params;
 
-
-termoRouter.post("/signup", async (req, res) => {
-  try {
-    const { password } = req.body;
-
-    if (
-      !password ||
-      !password.match(
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/gm
-      )
-    ) {
-      return res.status(400).json({
-        msg: "Email ou senha invalidos. Verifique se ambos atendem as requisições.",
-      });
+      const updatedTermo = await termoModel.findOneAndUpdate(
+        { _id: idTermo },
+        { ...req.body },
+        { new: true, runValidators: true }
+      );
+      return res.status(200).json(updatedTermo);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error.errors);
     }
+  });
 
-    const salt = await bcrypt.genSalt(SALT_ROUNDS);
-
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const createdUser = await UserModel.create({
-      ...req.body,
-      passwordHash: hashedPassword,
-    });
-
-    delete createdUser._doc.passwordHash;
-    return res.status(201).json(createdUser);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
-
-termoRouter.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await UserModel.findOne({ email: email });
-
-    if (!user) {
-      return res.status(404).json({ msg: "Email ou senha invalidos." });
+  //DELETE termo
+  termoRoute.delete("/delete/:idTermo", async (req, res) => {
+      try {
+        const { idTermo} = req.params;
+        const deletedTermo = await termoModel.findByIdAndDelete(idTermo);
+        return res.status(200).json(deletedTermo);
+      } catch (error) {
+        console.log(error);
+        return res.status(400).json(error.errors);
+      }
     }
+  );
 
-    if (await bcrypt.compare(password, user.passwordHash)) {
-      const token = generateToken(user);
 
-      return res.status(200).json({
-        user: {
-          name: user.name,
-          email: user.email,
-          _id: user._id,
-          role: user.role,
-        },
-        token: token,
-      });
-    } else {
-      return res.status(401).json({ msg: "Email ou senha invalidos." });
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
-
-// userRouter.get(
-//   "/teste",
-//   isAuth,
-//   attachCurrentUser,
-//   isAdmin,
-//   async (req, res) => {
-//     return res.status(200).json(req.currentUser);
-//   }
-// );
-
-export { termoRouter };
+export { termoRoute };
