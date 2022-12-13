4a137d1f0e1d74b6db78f60a492239da444be455
Angélica <a.r.g.eng.soft@gmail.com>
2022-12-13 14:40:55 -0300

    novo

--- routes/termo.routes.js
+++ routes/termo.routes.js
@@ -5,16 +5,16 @@ import { UserModel } from "../models/user.model.js";
 const termoRoute = express.Router();
 
 //CREATE termo
-termoRoute.post("/new-termo", async (req,res) => {
-    try {
-        const createdTermo = await termoModel.create({
-            ...req.body,
-          });
-          return res.status(201).json(createdTermo);
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
+termoRoute.post("/new-termo", async (req, res) => {
+  try {
+    const createdTermo = await termoModel.create({
+      ...req.body,
+    });
+    return res.status(201).json(createdTermo);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 });
 
 //READ em todos os termos
@@ -32,52 +32,49 @@ termoRoute.get("/all-termos", async (req, res) => {
 
 //READ termo específico - popula somente ou editadoPor ou criadoPor
 termoRoute.get("/termo/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      // const user = await UserModel.find({_id: id})
-      const termo = await termoModel.findById(id);
-      console.log("termo ",termo);
-      if (!termo) {
-        return res.status(400).json({ msg: " Termo não encontrado!" });
-      }
-      return res.status(200).json(termo);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json(error.errors);
+  try {
+    const { id } = req.params;
+
+    // const user = await UserModel.find({_id: id})
+    const termo = await termoModel.findById(id);
+    console.log("termo ", termo);
+    if (!termo) {
+      return res.status(400).json({ msg: " Termo não encontrado!" });
     }
-  });
+    return res.status(200).json(termo);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
 
 //UPDATE termo
 termoRoute.put("/edit/:idTermo", async (req, res) => {
   try {
     const { idTermo } = req.params;
 
-      const updatedTermo = await termoModel.findOneAndUpdate(
-        { _id: idTermo },
-        { ...req.body },
-        { new: true, runValidators: true }
-      );
-      return res.status(200).json(updatedTermo);
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json(error.errors);
-    }
-  });
+    const updatedTermo = await termoModel.findOneAndUpdate(
+      { _id: idTermo },
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+    return res.status(200).json(updatedTermo);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.errors);
+  }
+});
 
-  //DELETE termo
-  termoRoute.delete("/delete/:idTermo", async (req, res) => {
-      try {
-        const { idTermo} = req.params;
-        const deletedTermo = await termoModel.findByIdAndDelete(idTermo);
-        return res.status(200).json(deletedTermo);
-      } catch (error) {
-        console.log(error);
-        return res.status(400).json(error.errors);
-      }
-    }
-  );
+//DELETE termo
+termoRoute.delete("/delete/:idTermo", async (req, res) => {
+  try {
+    const { idTermo } = req.params;
+    const deletedTermo = await termoModel.findByIdAndDelete(idTermo);
+    return res.status(200).json(deletedTermo);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.errors);
+  }
+});
 
-
->>>>>>> master
 export { termoRoute };
