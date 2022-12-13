b590ac3ea49a867bc60ec1d9d992cb2d9d7df25d
Guilherme Oliveira <oliveiraguilherme@ufg.br>
2022-12-13 00:12:16 -0300

    1. adição dos arquivos e pastas que serão utilizados no projeto, 2. Alteração da disposição do index.

--- index.js
+++ index.js
@@ -2,19 +2,36 @@ import express from "express";
 import * as dotenv from "dotenv";
 import connect from "./config/db.config.js";
 
+//habilitar o servidor a ter variáveis de ambiente
 dotenv.config();
 
-const app = express() 
-app.use(express.json())
 
+//instanciar a variável que vai ficar responsável pelo nosso servidor
+const app = express() 
+
+<<<<<<< Updated upstream
 
 // lógicas das rotas vão aqui //
 
 connect();
+=======
+app.use(cors());
+>>>>>>> Stashed changes
+
+//configurar o servidor para aceitar enviar e receber arquivos em JSON
+app.use(express.json())
+
+<<<<<<< Updated upstream
+// app.use("/termo", termoRoute);
+=======
+// Conectando com banco de dados
+connect();
+
 
 // app.use("/user", userRoute);
-
-// app.use("/termo", termoRoute);
+// app.use("/log", logRoute);
+app.use("/termo", termoRoute );
+>>>>>>> Stashed changes
 
 // o servidor subindo pro ar.
 app.listen(process.env.PORT, () => {
