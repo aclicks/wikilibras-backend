e824dab9430e88026722b3600065f0acd99a057e
Guilherme Oliveira <oliveiraguilherme@ufg.br>
2022-12-13 00:31:47 -0300

    Revertendo alterações conflituosas

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
+import cors from "cors";
 import express from "express";
 import * as dotenv from "dotenv";
 import connect from "./config/db.config.js";
+import { termoRoute } from "./routes/termo.routes.js";
 
-//habilitar o servidor a ter variáveis de ambiente
 dotenv.config();
 
-
-//instanciar a variável que vai ficar responsável pelo nosso servidor
 const app = express() 
+app.use(express.json())
 
-<<<<<<< Updated upstream
 
 // lógicas das rotas vão aqui //
 
 connect();
-=======
 app.use(cors());
->>>>>>> Stashed changes
-
-//configurar o servidor para aceitar enviar e receber arquivos em JSON
-app.use(express.json())
-
-<<<<<<< Updated upstream
-// app.use("/termo", termoRoute);
-=======
-// Conectando com banco de dados
-connect();
-
 
 // app.use("/user", userRoute);
-// app.use("/log", logRoute);
+
 app.use("/termo", termoRoute );
->>>>>>> Stashed changes
 
 // o servidor subindo pro ar.
 app.listen(process.env.PORT, () => {
